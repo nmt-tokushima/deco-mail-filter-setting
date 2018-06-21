f626fbf302fc3b07152b1fd917912f938009dfaa
ka <ka.kaosf@gmail.com>
2018-06-21 17:35:36 +0900

    Modify the migration file

--- db/migrate/20180621083312_create_settings.rb
+++ db/migrate/20180621083312_create_settings.rb
@@ -1,14 +1,14 @@
 class CreateSettings < ActiveRecord::Migration[5.2]
   def change
     create_table :settings do |t|
-      t.boolean :bcc_conversion
-      t.string :bcc_dummy_to
-      t.boolean :attachments_encryption
-      t.integer :attachments_encryption_password_length
-      t.string :attachments_encryption_subtitle
-      t.text :attachments_encryption_additional_text
-      t.integer :attachments_encryption_encoding
-      t.boolean :attachments_encryption_password_notification
+      t.boolean :bcc_conversion, null: false, default: false
+      t.string :bcc_dummy_to, null: false, default: ''
+      t.boolean :attachments_encryption, null: false, default: false
+      t.integer :attachments_encryption_password_length, null: false, default: 8
+      t.string :attachments_encryption_subtitle, null: false, default: ''
+      t.text :attachments_encryption_additional_text, null: false
+      t.integer :attachments_encryption_encoding, null: false, default: 1
+      t.boolean :attachments_encryption_password_notification, null: false, default: false
 
       t.timestamps
     end
