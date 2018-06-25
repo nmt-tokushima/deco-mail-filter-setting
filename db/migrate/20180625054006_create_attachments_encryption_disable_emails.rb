a400c204438baf484e5d592bdaa7dfb39f29fef9
ka <ka.kaosf@gmail.com>
2018-06-25 14:41:36 +0900

    Modify the migration files

--- db/migrate/20180625054006_create_attachments_encryption_disable_emails.rb
+++ db/migrate/20180625054006_create_attachments_encryption_disable_emails.rb
@@ -1,8 +1,8 @@
 class CreateAttachmentsEncryptionDisableEmails < ActiveRecord::Migration[5.2]
   def change
     create_table :attachments_encryption_disable_emails do |t|
-      t.references :setting, foreign_key: true
-      t.string :email
+      t.references :setting, foreign_key: true, null: false
+      t.string :email, null: false
 
       t.timestamps
     end
--- db/migrate/20180625054008_create_attachments_encryption_disable_domain_tos.rb
+++ db/migrate/20180625054008_create_attachments_encryption_disable_domain_tos.rb
@@ -1,8 +1,8 @@
 class CreateAttachmentsEncryptionDisableDomainTos < ActiveRecord::Migration[5.2]
   def change
     create_table :attachments_encryption_disable_domain_tos do |t|
-      t.references :setting, foreign_key: true
-      t.string :domain
+      t.references :setting, foreign_key: true, null: false
+      t.string :domain, null: false
 
       t.timestamps
     end
