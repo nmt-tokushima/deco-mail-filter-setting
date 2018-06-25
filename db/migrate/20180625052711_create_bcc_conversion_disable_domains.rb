ea7f010cd43c6e79b8531b94824c63939a67e6e5
ka <ka.kaosf@gmail.com>
2018-06-25 14:36:54 +0900

    Modify the migration file

--- db/migrate/20180625052711_create_bcc_conversion_disable_domains.rb
+++ db/migrate/20180625052711_create_bcc_conversion_disable_domains.rb
@@ -1,8 +1,8 @@
 class CreateBccConversionDisableDomains < ActiveRecord::Migration[5.2]
   def change
     create_table :bcc_conversion_disable_domains do |t|
-      t.references :setting, foreign_key: true
-      t.string :domain
+      t.references :setting, foreign_key: true, null: false
+      t.string :domain, null: false
 
       t.timestamps
     end
