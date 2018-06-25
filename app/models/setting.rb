035874a854a035c9acc66dd1fdfab3eec49004d7
ka <ka.kaosf@gmail.com>
2018-06-25 15:47:22 +0900

    Update Setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,6 @@
 class Setting < ApplicationRecord
+  has_many :bcc_conversion_disable_domains, dependent: :destroy
+  has_many :attachments_encryption_disable_emails, dependent: :destroy
+  has_many :attachments_encryption_disable_domain_froms, dependent: :destroy
+  has_many :attachments_encryption_disable_domain_tos, dependent: :destroy
 end
