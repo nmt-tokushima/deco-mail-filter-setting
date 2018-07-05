38de4fc8a82b8d136c6ffbacd506edffea1528b5
ka <ka.kaosf@gmail.com>
2018-07-05 16:39:29 +0900

    Rename "subtitle" -> "subject"

--- db/migrate/20180705073834_rename_settings_attachments_encryption_subtitle.rb
+++ db/migrate/20180705073834_rename_settings_attachments_encryption_subtitle.rb
@@ -1,4 +1,5 @@
 class RenameSettingsAttachmentsEncryptionSubtitle < ActiveRecord::Migration[5.2]
   def change
+    rename_column :settings, :attachments_encryption_subtitle, :attachments_encryption_subject
   end
 end
