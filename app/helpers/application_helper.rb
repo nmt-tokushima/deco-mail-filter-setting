9d1fa362854589e448fdf5baac8d0a059e14a944
ka <ka.kaosf@gmail.com>
2018-07-09 22:34:42 +0900

    Fix views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def nav_item name, path, html_options = {}
+    "<li class=\"nav-item#{active? path}\">#{link_to name, path, html_options}</li>".html_safe
+  end
+
+  private
+
+  def active? path
+    if path == '/'
+      if request.path == '/'
+        " active"
+      else
+        ""
+      end
+    elsif /^#{path}/ =~ request.path
+      " active"
+    else
+      ""
+    end
+  end
 end
