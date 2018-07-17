2526eb706f047467ac617d7dc60b1eefc399206d
ka <ka.kaosf@gmail.com>
2018-07-17 18:05:22 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_072225) do
+ActiveRecord::Schema.define(version: 2018_07_17_085652) do
 
   create_table "api_access_allowed_ips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ip", null: false
@@ -69,6 +69,14 @@ ActiveRecord::Schema.define(version: 2018_07_17_072225) do
     t.integer "port", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "attachments_encryption_disable_domain_froms", "settings"
