c708f10b82ae7b482d492332177c1c974f67e433
ka <ka.kaosf@gmail.com>
2018-07-17 16:23:07 +0900

    Modify the migration file

--- db/migrate/20180717072225_create_api_access_allowed_ips.rb
+++ db/migrate/20180717072225_create_api_access_allowed_ips.rb
@@ -1,7 +1,7 @@
 class CreateApiAccessAllowedIps < ActiveRecord::Migration[5.2]
   def change
     create_table :api_access_allowed_ips do |t|
-      t.string :ip
+      t.string :ip, null: false
 
       t.timestamps
     end
